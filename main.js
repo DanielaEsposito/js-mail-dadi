5ff27ee8d810a2a2979ffe145ca6484a48f22b0f
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-10-17 14:04:48 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+//  Chiedi all'utente la sua email,
+//  controlla che sia nella lista di chi può accedere,
+//  stampa un messaggio appropriato sull'esito del controllo.
+
+
+
+// creo una lista di email che possono accedere
+
+// chiedo l'email all'utente
+
+// controllo che l'email sia valida in quanto email
+
+// SE l'emeil dell'utente è valida controllo che l'email dell'utente sia presente nella lista 
+
+// ALLORA SE l'email è presente nella lista mando un messaggio positivo all'utente
+
+// ALTRIMENTI se l'email non è valida comunico all'utente che l'email non è valida 
+
+
+
+
+
+//  Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+//  Stabilire il vincitore, in base a chi fa il punteggio più alto.
