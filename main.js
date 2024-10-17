4b88decfb3d1bf776080bcc8e6d8f3814d31fa63
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-10-17 14:41:52 +0200

    add email exercise

--- main.js
+++ main.js
@@ -6,13 +6,30 @@
 
 // creo una lista di email che possono accedere
 
+const emails =["myemail@gmail.com", "gianni@gmail.com", "paolo@gmcil.com", "piero@gmail.com"]
+
 // chiedo l'email all'utente
+
+const userEmail = prompt("scrivi qui la tua email");
 
 // controllo che l'email sia valida in quanto email
 
-// SE l'emeil dell'utente è valida controllo che l'email dell'utente sia presente nella lista 
+const isUserEmailValid = userEmail.includes("@") && userEmail.includes(".");
 
-// ALLORA SE l'email è presente nella lista mando un messaggio positivo all'utente
+// SE l'emeil dell'utente è valida controllo che l'email dell'utente sia presente nella lista mando un messaggio
+
+if(isUserEmailValid){
+
+    for (let i = 0; i < emails.length ; i ++){
+        userEmail === emails[i];
+        console.log("puoi accedere");
+        alert("puoi accedere")   
+    }
+}
+else{
+    console.log("Email non valida!");
+    alert("Email non valida !")
+}
 
 // ALTRIMENTI se l'email non è valida comunico all'utente che l'email non è valida 
 
